bfbfad9b257f3019b729c2e22e1f3330a3ee9851
Ali Ustek <13117393+austek@users.noreply.github.com>
2024-05-23 23:20:26 +0100

    Add .tar.xz and .txz to FileType (#3)

---
+++ src/test/java/org/compress4j/archivers/ArchiverTarXzTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2024 The Compress4J Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.compress4j.archivers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import org.junit.jupiter.api.Test;
+
+class ArchiverTarXzTest extends AbstractArchiverTest {
+
+    @Override
+    protected Archiver getArchiver() {
+        return ArchiverFactory.createArchiver(ArchiveFormat.TAR, CompressionType.XZ);
+    }
+
+    @Override
+    protected File getArchive() {
+        return new File(RESOURCES_DIR, "archive.tar.xz");
+    }
+
+    @Test
+    void getFilenameExtension_tar_xz_returnsCorrectFilenameExtension() {
+        assertThat(getArchiver().getFilenameExtension()).isEqualTo(".tar.xz");
+    }
+}
