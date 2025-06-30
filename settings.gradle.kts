5027d0edd27851614c5ff52b1861d6039653836e
renasustek <renasustek@gmail.com>
2025-06-30 14:41:24 +0100

    pr review

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "compress4j"
 
 plugins {
     id("com.gradle.develocity") version("3.19")
-    id("com.autonomousapps.build-health") version "2.17.0"
+    id("com.autonomousapps.build-health") version "2.19.0"
 }
 
 develocity {
