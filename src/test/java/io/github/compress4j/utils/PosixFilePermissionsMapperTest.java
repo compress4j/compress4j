fda0e97d96178920472c9b92c6fbec1fc31d0348
austek <13117393+austek@users.noreply.github.com>
2025-07-25 22:44:10 +0100

    Suppress TestCases should contain tests for PosixFilePermissionsMapperTest

--- src/test/java/io/github/compress4j/utils/PosixFilePermissionsMapperTest.java
+++ src/test/java/io/github/compress4j/utils/PosixFilePermissionsMapperTest.java
@@ -15,6 +15,8 @@
  */
 package io.github.compress4j.utils;
 
+import static io.github.compress4j.utils.PosixFilePermissionsMapper.fromUnixMode;
+import static io.github.compress4j.utils.PosixFilePermissionsMapper.toUnixMode;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_EXECUTE;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_READ;
 import static java.nio.file.attribute.PosixFilePermission.GROUP_WRITE;
@@ -33,7 +35,7 @@ import java.util.Set;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("OctalInteger")
+@SuppressWarnings({"java:S2187", "OctalInteger"})
 class PosixFilePermissionsMapperTest {
 
     @Nested
@@ -44,68 +46,62 @@ class PosixFilePermissionsMapperTest {
 
         @Test
         void noPermissions() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0000)).isEmpty();
+            assertThat(fromUnixMode(0000)).isEmpty();
         }
 
         @Test
         void allPermissions() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0777)).containsAll(setOf(PosixFilePermission.values()));
+            assertThat(fromUnixMode(0777)).containsAll(setOf(PosixFilePermission.values()));
         }
 
         @Test
         void ownerPermissions() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0400)).containsAll(setOf(OWNER_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0200)).containsAll(setOf(OWNER_WRITE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0100)).containsAll(setOf(OWNER_EXECUTE));
+            assertThat(fromUnixMode(0400)).containsAll(setOf(OWNER_READ));
+            assertThat(fromUnixMode(0200)).containsAll(setOf(OWNER_WRITE));
+            assertThat(fromUnixMode(0100)).containsAll(setOf(OWNER_EXECUTE));
 
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0700))
-                    .containsAll(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0600)).containsAll(setOf(OWNER_WRITE, OWNER_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0500)).containsAll(setOf(OWNER_EXECUTE, OWNER_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0300)).containsAll(setOf(OWNER_EXECUTE, OWNER_WRITE));
+            assertThat(fromUnixMode(0700)).containsAll(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE));
+            assertThat(fromUnixMode(0600)).containsAll(setOf(OWNER_WRITE, OWNER_READ));
+            assertThat(fromUnixMode(0500)).containsAll(setOf(OWNER_EXECUTE, OWNER_READ));
+            assertThat(fromUnixMode(0300)).containsAll(setOf(OWNER_EXECUTE, OWNER_WRITE));
         }
 
         @Test
         void groupPermissions() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0040)).containsAll(setOf(GROUP_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0020)).containsAll(setOf(GROUP_WRITE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0010)).containsAll(setOf(GROUP_EXECUTE));
+            assertThat(fromUnixMode(0040)).containsAll(setOf(GROUP_READ));
+            assertThat(fromUnixMode(0020)).containsAll(setOf(GROUP_WRITE));
+            assertThat(fromUnixMode(0010)).containsAll(setOf(GROUP_EXECUTE));
 
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0070))
-                    .containsAll(setOf(GROUP_READ, GROUP_WRITE, GROUP_EXECUTE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0060)).containsAll(setOf(GROUP_WRITE, GROUP_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0050)).containsAll(setOf(GROUP_EXECUTE, GROUP_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0030)).containsAll(setOf(GROUP_EXECUTE, GROUP_WRITE));
+            assertThat(fromUnixMode(0070)).containsAll(setOf(GROUP_READ, GROUP_WRITE, GROUP_EXECUTE));
+            assertThat(fromUnixMode(0060)).containsAll(setOf(GROUP_WRITE, GROUP_READ));
+            assertThat(fromUnixMode(0050)).containsAll(setOf(GROUP_EXECUTE, GROUP_READ));
+            assertThat(fromUnixMode(0030)).containsAll(setOf(GROUP_EXECUTE, GROUP_WRITE));
         }
 
         @Test
         void othersPermissions() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0004)).containsAll(setOf(OTHERS_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0002)).containsAll(setOf(OTHERS_WRITE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0001)).containsAll(setOf(OTHERS_EXECUTE));
+            assertThat(fromUnixMode(0004)).containsAll(setOf(OTHERS_READ));
+            assertThat(fromUnixMode(0002)).containsAll(setOf(OTHERS_WRITE));
+            assertThat(fromUnixMode(0001)).containsAll(setOf(OTHERS_EXECUTE));
 
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0007))
-                    .containsAll(setOf(OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0006)).containsAll(setOf(OTHERS_WRITE, OTHERS_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0005)).containsAll(setOf(OTHERS_EXECUTE, OTHERS_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0003)).containsAll(setOf(OTHERS_EXECUTE, OTHERS_WRITE));
+            assertThat(fromUnixMode(0007)).containsAll(setOf(OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE));
+            assertThat(fromUnixMode(0006)).containsAll(setOf(OTHERS_WRITE, OTHERS_READ));
+            assertThat(fromUnixMode(0005)).containsAll(setOf(OTHERS_EXECUTE, OTHERS_READ));
+            assertThat(fromUnixMode(0003)).containsAll(setOf(OTHERS_EXECUTE, OTHERS_WRITE));
         }
 
         @Test
         void permissionsSameForAll() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0444))
-                    .containsAll(setOf(OTHERS_READ, GROUP_READ, OWNER_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0222))
-                    .containsAll(setOf(OTHERS_WRITE, GROUP_WRITE, OWNER_WRITE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0111))
-                    .containsAll(setOf(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE));
+            assertThat(fromUnixMode(0444)).containsAll(setOf(OTHERS_READ, GROUP_READ, OWNER_READ));
+            assertThat(fromUnixMode(0222)).containsAll(setOf(OTHERS_WRITE, GROUP_WRITE, OWNER_WRITE));
+            assertThat(fromUnixMode(0111)).containsAll(setOf(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE));
         }
 
         @Test
         void permissionCombinations() {
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0750))
+            assertThat(fromUnixMode(0750))
                     .containsAll(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE, GROUP_READ, GROUP_EXECUTE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0753))
+            assertThat(fromUnixMode(0753))
                     .containsAll(setOf(
                             OWNER_READ,
                             OWNER_WRITE,
@@ -114,12 +110,10 @@ class PosixFilePermissionsMapperTest {
                             GROUP_EXECUTE,
                             OTHERS_WRITE,
                             OTHERS_EXECUTE));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0574))
+            assertThat(fromUnixMode(0574))
                     .containsAll(setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, GROUP_WRITE, GROUP_EXECUTE, OTHERS_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0544))
-                    .containsAll(setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, OTHERS_READ));
-            assertThat(PosixFilePermissionsMapper.fromUnixMode(0055))
-                    .containsAll(setOf(GROUP_READ, GROUP_EXECUTE, OTHERS_READ, OTHERS_EXECUTE));
+            assertThat(fromUnixMode(0544)).containsAll(setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, OTHERS_READ));
+            assertThat(fromUnixMode(0055)).containsAll(setOf(GROUP_READ, GROUP_EXECUTE, OTHERS_READ, OTHERS_EXECUTE));
         }
     }
 
@@ -131,86 +125,67 @@ class PosixFilePermissionsMapperTest {
 
         @Test
         void noPermissions() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(Set.of())).isZero();
+            assertThat(toUnixMode(Set.of())).isZero();
         }
 
         @Test
         void allPermissions() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(PosixFilePermission.values())))
-                    .isEqualTo(0777);
+            assertThat(toUnixMode(setOf(PosixFilePermission.values()))).isEqualTo(0777);
         }
 
         @Test
         void ownerPermissions() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_READ))).isEqualTo(0400);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_WRITE)))
-                    .isEqualTo(0200);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_EXECUTE)))
-                    .isEqualTo(0100);
+            assertThat(toUnixMode(setOf(OWNER_READ))).isEqualTo(0400);
+            assertThat(toUnixMode(setOf(OWNER_WRITE))).isEqualTo(0200);
+            assertThat(toUnixMode(setOf(OWNER_EXECUTE))).isEqualTo(0100);
 
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE)))
+            assertThat(toUnixMode(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE)))
                     .isEqualTo(0700);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_WRITE, OWNER_READ)))
-                    .isEqualTo(0600);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_EXECUTE, OWNER_READ)))
-                    .isEqualTo(0500);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_EXECUTE, OWNER_WRITE)))
-                    .isEqualTo(0300);
+            assertThat(toUnixMode(setOf(OWNER_WRITE, OWNER_READ))).isEqualTo(0600);
+            assertThat(toUnixMode(setOf(OWNER_EXECUTE, OWNER_READ))).isEqualTo(0500);
+            assertThat(toUnixMode(setOf(OWNER_EXECUTE, OWNER_WRITE))).isEqualTo(0300);
         }
 
         @Test
         void groupPermissions() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_READ))).isEqualTo(0040);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_WRITE)))
-                    .isEqualTo(0020);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_EXECUTE)))
-                    .isEqualTo(0010);
+            assertThat(toUnixMode(setOf(GROUP_READ))).isEqualTo(0040);
+            assertThat(toUnixMode(setOf(GROUP_WRITE))).isEqualTo(0020);
+            assertThat(toUnixMode(setOf(GROUP_EXECUTE))).isEqualTo(0010);
 
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_READ, GROUP_WRITE, GROUP_EXECUTE)))
+            assertThat(toUnixMode(setOf(GROUP_READ, GROUP_WRITE, GROUP_EXECUTE)))
                     .isEqualTo(0070);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_WRITE, GROUP_READ)))
-                    .isEqualTo(0060);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_EXECUTE, GROUP_READ)))
-                    .isEqualTo(0050);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(GROUP_EXECUTE, GROUP_WRITE)))
-                    .isEqualTo(0030);
+            assertThat(toUnixMode(setOf(GROUP_WRITE, GROUP_READ))).isEqualTo(0060);
+            assertThat(toUnixMode(setOf(GROUP_EXECUTE, GROUP_READ))).isEqualTo(0050);
+            assertThat(toUnixMode(setOf(GROUP_EXECUTE, GROUP_WRITE))).isEqualTo(0030);
         }
 
         @Test
         void othersPermissions() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_READ)))
-                    .isEqualTo(0004);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_WRITE)))
-                    .isEqualTo(0002);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_EXECUTE)))
-                    .isEqualTo(0001);
+            assertThat(toUnixMode(setOf(OTHERS_READ))).isEqualTo(0004);
+            assertThat(toUnixMode(setOf(OTHERS_WRITE))).isEqualTo(0002);
+            assertThat(toUnixMode(setOf(OTHERS_EXECUTE))).isEqualTo(0001);
 
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE)))
+            assertThat(toUnixMode(setOf(OTHERS_READ, OTHERS_WRITE, OTHERS_EXECUTE)))
                     .isEqualTo(0007);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_WRITE, OTHERS_READ)))
-                    .isEqualTo(0006);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_EXECUTE, OTHERS_READ)))
-                    .isEqualTo(0005);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_EXECUTE, OTHERS_WRITE)))
-                    .isEqualTo(0003);
+            assertThat(toUnixMode(setOf(OTHERS_WRITE, OTHERS_READ))).isEqualTo(0006);
+            assertThat(toUnixMode(setOf(OTHERS_EXECUTE, OTHERS_READ))).isEqualTo(0005);
+            assertThat(toUnixMode(setOf(OTHERS_EXECUTE, OTHERS_WRITE))).isEqualTo(0003);
         }
 
         @Test
         void permissionsSameForAll() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_READ, GROUP_READ, OWNER_READ)))
-                    .isEqualTo(0444);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_WRITE, GROUP_WRITE, OWNER_WRITE)))
+            assertThat(toUnixMode(setOf(OTHERS_READ, GROUP_READ, OWNER_READ))).isEqualTo(0444);
+            assertThat(toUnixMode(setOf(OTHERS_WRITE, GROUP_WRITE, OWNER_WRITE)))
                     .isEqualTo(0222);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE)))
+            assertThat(toUnixMode(setOf(OTHERS_EXECUTE, GROUP_EXECUTE, OWNER_EXECUTE)))
                     .isEqualTo(0111);
         }
 
         @Test
         void permissionCombinations() {
-            assertThat(PosixFilePermissionsMapper.toUnixMode(
-                            setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE, GROUP_READ, GROUP_EXECUTE)))
+            assertThat(toUnixMode(setOf(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE, GROUP_READ, GROUP_EXECUTE)))
                     .isEqualTo(0750);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(
+            assertThat(toUnixMode(setOf(
                             OWNER_READ,
                             OWNER_WRITE,
                             OWNER_EXECUTE,
@@ -219,13 +194,12 @@ class PosixFilePermissionsMapperTest {
                             OTHERS_WRITE,
                             OTHERS_EXECUTE)))
                     .isEqualTo(0753);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(
+            assertThat(toUnixMode(
                             setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, GROUP_WRITE, GROUP_EXECUTE, OTHERS_READ)))
                     .isEqualTo(0574);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, OTHERS_READ)))
+            assertThat(toUnixMode(setOf(OWNER_READ, OWNER_EXECUTE, GROUP_READ, OTHERS_READ)))
                     .isEqualTo(0544);
-            assertThat(PosixFilePermissionsMapper.toUnixMode(
-                            setOf(GROUP_READ, GROUP_EXECUTE, OTHERS_READ, OTHERS_EXECUTE)))
+            assertThat(toUnixMode(setOf(GROUP_READ, GROUP_EXECUTE, OTHERS_READ, OTHERS_EXECUTE)))
                     .isEqualTo(0055);
         }
     }
