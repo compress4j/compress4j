de42cd4413971b82dc5cac56af89c7e0468ccc57
renasustek <renasustek@gmail.com>
2025-07-26 16:43:03 +0100

    fix bzip2 e2e test

---
+++ src/e2eTest/java/io/github/compress4j/compressors/BZip2Test.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2025 The Compress4J Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.compress4j.compressors;
+
+import static io.github.compress4j.assertion.Compress4JAssertions.assertThat;
+import static io.github.compress4j.test.util.io.TestFileUtils.createFile;
+import org.assertj.core.api.Assertions;
+
+import io.github.compress4j.compressors.bzip2.BZip2Compressor;
+import io.github.compress4j.compressors.bzip2.BZip2Decompressor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class BZip2Test {
+
+    @TempDir
+    Path tempDir;
+
+    Path sourcePath;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        sourcePath = createFile(tempDir, "sourcePath", "compressMe");
+    }
+
+    @Test
+    void compressDecompressSameFile() throws Exception {
+        var compressPath = tempDir.resolve("compressTest.txt.bz");
+        var decompressPath = tempDir.resolve("decompressedTest.txt");
+
+        try (BZip2Compressor bZip2Compressor = BZip2Compressor.builder(compressPath).build()) {
+            bZip2Compressor.write(sourcePath);
+        }
+
+        assertThat(compressPath).exists();
+
+        try (BZip2Decompressor bZip2Decompressor =
+                BZip2Decompressor.builder(compressPath).build()) {
+            bZip2Decompressor.write(decompressPath);
+        }
+
+        assertThat(decompressPath).exists();
+        Assertions.assertThat(FileUtils.readFileToString(sourcePath.toFile(), "UTF-8"))
+                .isEqualTo(FileUtils.readFileToString(decompressPath.toFile(), "UTF-8"));
+    }
+
+    @Test
+    void compressWithParametersThenDecompress() throws Exception {
+        var compressPath = tempDir.resolve("compressTest.txt.bz");
+        var decompressPath = tempDir.resolve("decompressedTest.txt");
+
+        try (BZip2Compressor bZip2Compressor = BZip2Compressor.builder(compressPath)
+                .compressorOutputStreamBuilder()
+                .blockSize(6)
+                .parentBuilder()
+                .build()) {
+            bZip2Compressor.write(sourcePath);
+        }
+
+        assertThat(compressPath).exists();
+
+
+        try (BZip2Decompressor bZip2Decompressor =
+                BZip2Decompressor.builder(compressPath).build()) {
+            bZip2Decompressor.write(decompressPath);
+        }
+
+        assertThat(decompressPath).exists();
+
+        Assertions.assertThat(FileUtils.readFileToString(sourcePath.toFile(), "UTF-8"))
+                .isEqualTo(FileUtils.readFileToString(decompressPath.toFile(), "UTF-8"));
+    }
+}
