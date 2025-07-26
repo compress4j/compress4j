ed718a20a6b00e2968c9d7ea74678a463ab3dea8
renasustek <renasustek@gmail.com>
2025-07-26 17:36:21 +0100

    delate extend abstact

--- src/e2eTest/java/io/github/compress4j/compressors/DeflateTest.java
+++ src/e2eTest/java/io/github/compress4j/compressors/DeflateTest.java
@@ -15,50 +15,22 @@
  */
 package io.github.compress4j.compressors;
 
-import static io.github.compress4j.assertion.Compress4JAssertions.assertThat;
-import static io.github.compress4j.test.util.io.TestFileUtils.createFile;
-
 import io.github.compress4j.compressors.deflate.DeflateCompressor;
 import io.github.compress4j.compressors.deflate.DeflateDecompressor;
+
 import java.io.IOException;
 import java.nio.file.Path;
-import org.apache.commons.io.FileUtils;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
-class DeflateTest {
+class DeflateTest extends AbstractTest{
 
-    @TempDir
-    Path tempDir;
 
-    Path sourcePath;
-
-    @BeforeEach
-    void setUp() throws IOException {
-        sourcePath = createFile(tempDir, "sourcePath", "compressMe");
+    @Override
+    protected Compressor<?> compressorBuilder(Path compressPath) throws IOException {
+        return new DeflateCompressor.DeflateCompressorBuilder(compressPath).build();
     }
 
-    @Test
-    void compressDecompressSameFile() throws Exception {
-        var compressPath = tempDir.resolve("compressTest.txt.bz");
-        var decompressPath = tempDir.resolve("decompressedTest.txt");
-
-        try (DeflateCompressor deflateCompressor =
-                DeflateCompressor.builder(compressPath).build()) {
-            deflateCompressor.write(sourcePath);
-        }
-
-        assertThat(compressPath).exists();
-
-        try (DeflateDecompressor deflateDecompressor =
-                DeflateDecompressor.builder(compressPath).build()) {
-            deflateDecompressor.write(decompressPath);
-        }
-
-        assertThat(decompressPath).exists();
-        Assertions.assertThat(FileUtils.readFileToString(sourcePath.toFile(), "UTF-8"))
-                .isEqualTo(FileUtils.readFileToString(decompressPath.toFile(), "UTF-8"));
+    @Override
+    protected Decompressor<?> decompressorBuilder(Path compressPath) throws IOException {
+        return new DeflateDecompressor.DeflateDecompressorBuilder(compressPath).build();
     }
 }
