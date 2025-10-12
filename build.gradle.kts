556fdd373cc0ef5a26cb70196323681b3125b460
renasustek <renasustek@gmail.com>
2025-10-12 15:01:14 +0100

    revert incorrect deletion

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     testFixturesApi(libs.commons.compress)
     testFixturesApi(libs.jackson.core)
     testFixturesApi(libs.jakarta.annotation.api)
+    testFixturesApi(libs.junit.jupiter.api)
     testFixturesApi(libs.logback.classic)
     testFixturesApi(libs.logback.core)
 
