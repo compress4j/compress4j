ff59313334d2085cbdf7d3b775af3fb7b5198c91
austek <13117393+austek@users.noreply.github.com>
2025-10-19 22:32:48 +0100

    Merge Integration and e22 tests and remove the e2eTest configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "compress4j"
 
 plugins {
-    id("com.gradle.develocity") version("4.1")
+    id("com.gradle.develocity") version("4.2.2")
     id("com.autonomousapps.build-health") version "2.19.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
