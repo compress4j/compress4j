80055bd04636e9e2cdeaa776bb5c5e70718f1eeb
austek <13117393+austek@users.noreply.github.com>
2026-02-01 02:42:52 +0000

    Use foojay

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -1,4 +1,8 @@
 // https://github.com/gradle/gradle/issues/30299
 rootProject.name = "plugins"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 dependencyResolutionManagement { versionCatalogs { create("libs") { from(files("../libs.versions.toml")) } } }
